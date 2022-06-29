5c8aec9e2998de77cae47a00b9085e14cd928ecb
Сережа Присяжнюк <priseajniuc2000@mail.ru>
2022-06-29 10:08:31 +0300

    create enum with coding keys

--- MilestoneAPI/MilestoneAPI/Model/Movie.swift
+++ MilestoneAPI/MilestoneAPI/Model/Movie.swift
@@ -13,33 +13,50 @@ struct MovieResponse: Codable {
 
 struct Movie: Codable {
     let id: Int
-    let original_title: String
-    let poster_path: String
-    let release_date: String
-    let vote_average: String
+    let title: String?
+    let original_title: String?
+    let poster_path: String?
+    let overview: String?
     let vote_count: Int
+    let release_date: String?
+    let vote_average: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case title = "title"
+        case original_title = "original_title"
+        case poster_path = "poster_path"
+        case overview = "overview"
+        case vote_count = "vote_count"
+        case release_date = "release_date"
+        case vote_average = "vote_average"
+    }
 }
 
 /*
- results =     (
-             {
-         adult = 0;
-         "backdrop_path" = "/wcKFYIiVDvRURrzglV9kGu7fpfY.jpg";
-         "genre_ids" =             (
-             14,
-             28,
-             12
-         );
-         id = 453395;
-         "original_language" = en;
-         "original_title" = "Doctor Strange in the Multiverse of Madness";
-         overview = "Doctor Strange, with the help of mystical allies both old and new, traverses the mind-bending and dangerous alternate realities of the Multiverse to confront a mysterious new adversary.";
-         popularity = "20983.423";
-         "poster_path" = "/9Gtg2DzBhmYamXBS1hKAhiwbBKS.jpg";
-         "release_date" = "2022-05-04";
-         title = "Doctor Strange in the Multiverse of Madness";
-         video = 0;
-         "vote_average" = "7.5";
-         "vote_count" = 3408;
-     }
+ 
+ {
+     page = 1;
+     results =     (
+                 {
+             adult = 0;
+             "backdrop_path" = "/wcKFYIiVDvRURrzglV9kGu7fpfY.jpg";
+             "genre_ids" =             (
+                 14,
+                 28,
+                 12
+             );
+             id = 453395;
+             "original_language" = en;
+             "original_title" = "Doctor Strange in the Multiverse of Madness";
+             overview = "Doctor Strange, with the help of mystical allies both old and new, traverses the mind-bending and dangerous alternate realities of the Multiverse to confront a mysterious new adversary.";
+             popularity = "12833.993";
+             "poster_path" = "/9Gtg2DzBhmYamXBS1hKAhiwbBKS.jpg";
+             "release_date" = "2022-05-04";
+             title = "Doctor Strange in the Multiverse of Madness";
+             video = 0;
+             "vote_average" = "7.5";
+             "vote_count" = 3596;
+         }
+ 
  */
